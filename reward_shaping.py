7691abe8103a3bdf729efe0c6f5e8e7c44154456
Manish Saroya <manishsaroya@gmail.com>
2018-11-02 13:16:10 -0700

    made training functional

--- reward_shaping.py
+++ reward_shaping.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 import copy
 
 # Number of Agents
-numAgents = 30
+numAgents = 50
 
 # Number of Nights 
-numNights = 7
+numNights = 5
 
 # Optimal Number of agents at each night
 numOptimalAgents = 5
 
 # learning rate 
-alpha = 0.1
+alpha = 0.05
 
 # exploration rate
 epsilon = 0.1
@@ -48,8 +48,7 @@ def updateGlobalRewardEstimates(attendance,actions):
     for agent, action in enumerate(actions):
         #print attendance[int(action)]
         #print (-1 * attendance[int(action)])/numOptimalAgents
-        #sample = np.exp((-1 * attendance[int(action)])/numOptimalAgents)
-        
+        #sample = np.exp((-1 * attendance[int(action)])/numOptimalAgents)        
         rewardEstimates[agent][int(action)] = (1-alpha) * rewardEstimates[agent][int(action)] + alpha * sample
 
 def counterfactualAction(attendance,actions,agent):
@@ -67,20 +66,12 @@ def counterfactualActionAbsence(attendance,actions,agent):
     return attendance, actions
 
 def updateDifferenceRewardEstimates(attendance,actions):
-    #print "in func",actions,attendance
     globalSample = computeGlobalReward(attendance,actions)
     for agent, action in enumerate(actions):
-        #print attendance[int(action)]
-        #print (-1 * attendance[int(action)])/numOptimalAgents
-        #sample = np.exp((-1 * attendance[int(action)])/numOptimalAgents)
-        #print "pre atten", attendance
+        #print "pre" , attendance, actions
         atte, act = counterfactualAction(copy.copy(attendance),copy.copy(actions),agent)
-        #print "compare actions",cmp(act,actions)
-        #print "compare attendance", cmp(atte,attendance)
-        #print "counterfact" ,atte, act
-        #print "orig" , attendance ,actions
+        #print "post" , atte, act
         sample = globalSample - computeGlobalReward(atte,act)
-        #print sample
         rewardEstimates[agent][int(action)] = (1-alpha) * rewardEstimates[agent][int(action)] + alpha * sample
 
 def updateDifferenceRewardEstimatesAbsence(attendance,actions):
@@ -97,15 +88,14 @@ def computeGlobalReward(attendance,actions):
     return globalReward
 
 
-# Visualization
-fig = plt.figure(figsize=(4, 4))
-#im = plt.imshow(, origin={'lower','left'})
-
-if __name__ == '__main__':
-    print rewardEstimates
+def trainDifferenceReward(iterations):
+    global rewardEstimates 
+    rewardEstimates = np.zeros((numAgents, numNights))
+    global epsilon
+    epsilon = 0.1
     rewardList = []
     a = []
-    for i in range(500):
+    for i in range(iterations):
         atten, actions = takeActions()
         a = atten
         #updateLocalRewardEstimates(atten,actions)
@@ -113,21 +103,24 @@ if __name__ == '__main__':
         updateDifferenceRewardEstimates(atten, actions)
         rewardList.append(computeGlobalReward(atten,actions))
         epsilon *= 0.999
-        print epsilon
+        #print epsilon
     #print rewardEstimates 
     print rewardList
-    print "Difference", a
+    print "Difference random", a
     plt.plot(rewardList,label='Difference Reward c_i-random action')
     plt.ylabel('Global Reward')
     plt.xlabel('No. of Episodes')
 
+def trainGlobalReward(iterations):
+    global rewardEstimates 
     rewardEstimates = np.zeros((numAgents, numNights))
+    global epsilon
     epsilon = 0.1
 
     # Making all plots in one
     rewardList1 = []
     a1 = []
-    for i in range(500):
+    for i in range(1000):
         atten, actions = takeActions()
         a1 = atten
         #updateLocalRewardEstimates(atten,actions)
@@ -135,18 +128,21 @@ if __name__ == '__main__':
         #updateDifferenceRewardEstimates(atten, actions)
         rewardList1.append(computeGlobalReward(atten,actions))
         epsilon *= 0.999
-        print epsilon
-    print "Global", a1 
+        #print epsilon
+    print "Global", a1
     plt.plot(rewardList1,label='Global Reward')
 
 
+def trainLocalReward(iterations):
+    global rewardEstimates
     rewardEstimates = np.zeros((numAgents, numNights))
+    global epsilon
     epsilon = 0.1
 
     # Making all plots in one
     rewardList2 = []
     a2 = []
-    for i in range(500):
+    for i in range(iterations):
         atten, actions = takeActions()
         a2 = atten
         updateLocalRewardEstimates(atten,actions)
@@ -154,18 +150,21 @@ if __name__ == '__main__':
         #updateDifferenceRewardEstimates(atten, actions)
         rewardList2.append(computeGlobalReward(atten,actions))
         epsilon *= 0.999
-        print epsilon
+        #print epsilon
     print "Local", a2
     plt.plot(rewardList2,label='Local Reward')
 
 
+def trainDifferenceAbsenceReward(iterations):
+    global rewardEstimates
     rewardEstimates = np.zeros((numAgents, numNights))
+    global epsilon
     epsilon = 0.1
 
     # Making all plots in one
     rewardList3 = []
     a3 = []
-    for i in range(500):
+    for i in range(iterations):
         atten, actions = takeActions()
         a3 = atten
         #updateLocalRewardEstimates(atten,actions)
@@ -173,21 +172,20 @@ if __name__ == '__main__':
         updateDifferenceRewardEstimatesAbsence(atten, actions)
         rewardList3.append(computeGlobalReward(atten,actions))
         epsilon *= 0.999
-        print epsilon
-    print "Local", a3
+        #print epsilon
+    print "Difference no action", a3
     plt.plot(rewardList3,label='Difference Reward c_i-No action')
 
-    # ploting the histogram
-    # fixed bin size
-    #plt.figure(figsize=(12, 12))
-    #bins = np.arange(0, numNights, 1) # fixed bin size
+# Visualization
+fig = plt.figure(figsize=(4, 4))
+#im = plt.imshow(, origin={'lower','left'})
 
-    #plt.xlim([min(a)-5, max(a)+5])
-
-    #plt.hist(a, bins=bins, alpha=0.5)
-    #plt.title('Random Gaussian data (fixed bin size)')
-    #plt.xlabel('variable X (bin size = 5)')
-    #plt.ylabel('count')
+if __name__ == '__main__':
+    print rewardEstimates
+    trainDifferenceReward(1000)
+    trainGlobalReward(1000)
+    trainLocalReward(1000)
+    trainDifferenceAbsenceReward(1000)
 plt.legend()    
 plt.show()
 
